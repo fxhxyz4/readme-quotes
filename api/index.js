18b9b0b614384e61e122c7262d7c213e29b7b6fe
fxhxyz <fxhxyz@proton.me>
2023-11-06 00:27:25 +0200

    -

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const renderQuote = q => {
       <foreignObject  width="100%" height="100%">
         <div xmlns="http://www.w3.org/1999/xhtml" style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center;">
           <h3 style="font-size: 22px;">“${quote}”</h3>
-          <div style="font-size: 18px; position: absolute;  right: 0">
+          <div style="font-size: 18px; position: absolute;  right: 0; bottom: 0;">
             <h3>— ${author}</h3>
           </div>
         </div>
