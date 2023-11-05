64befc3724f84129e1bb86314b761e9bffefcd6c
fxhxyz <fxhxyz@proton.me>
2023-11-06 01:54:31 +0200

    -

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ const renderQuote = q => {
   return `
     <svg width="800" height="200" fill="#000000" xmlns="http://www.w3.org/2000/svg">
       <foreignObject  width="100%" height="100%">
-        <div xmlns="http://www.w3.org/1999/xhtml" style="color: #1f1f1f; display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center;">
+        <div xmlns="http://www.w3.org/1999/xhtml" style="color: #2b2b2b; display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center;">
           <h3 style="font-size: 22px;">“${quote}”</h3>
           <div style="color: #000000; font-size: 18px; position: absolute; right: 0; bottom: 0;">
             <h3>— ${author}</h3>
