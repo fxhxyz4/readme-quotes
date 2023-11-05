4c5b743fad35646891fe1bab878a5eecc771632d
fxhxyz <fxhxyz@proton.me>
2023-11-06 01:00:11 +0200

    -

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default async function handler(req, res) {
   let fetchUrl = 'https://dev-quotes.onrender.com/api/id?id=445';
 
-  const { api2 } = req.query;
+  const api2 = req.query.api2;
 
   switch (api2) {
     case 'id':
