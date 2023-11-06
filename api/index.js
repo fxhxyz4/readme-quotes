1bf9cb6758fb81338ae268582ea3b12c0370a3e5
fxhxyz <fxhxyz@proton.me>
2023-11-06 02:14:56 +0200

    -

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
   const quote = await fetchQuotes(fetchUrl);
 
   res.setHeader('Content-Type', 'image/svg+xml');
-  res.send(renderQuote(quote));
+  res.send(renderQuote(req.query));
 }
 
 const fetchQuotes = async url => {
@@ -34,7 +34,7 @@ const renderQuote = q => {
     <svg width="800" height="200" fill="none" xmlns="http://www.w3.org/2000/svg">
       <foreignObject  width="100%" height="100%">
         <div xmlns="http://www.w3.org/1999/xhtml" style="color: ${cl.color1}; display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center;">
-          <h3 style="font-size: 22px;">“${req.query}”</h3>
+          <h3 style="font-size: 22px;">“${quote}”</h3>
           <div style="color: ${cl.color2}; font-size: 18px; position: absolute; right: 0; bottom: 0;">
             <h3>— ${author}</h3>
           </div>
